864b09ac38d820cf2b2a3c163b86f723b586cdb2
Arushi Sthapak <99334415+Arushi-Sthapak@users.noreply.github.com>
2024-12-30 20:12:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
-# Install libnss3
-apt-get update
-apt-get install -y libnss3
+# Download the latest version of chromedriver compatible with your environment
+CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/local/bin/chromedriver
+chmod +x /usr/local/bin/chromedriver
+
