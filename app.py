426a66df45893e99a376193e48fa13b9b54f2435
Arushi Sthapak <99334415+Arushi-Sthapak@users.noreply.github.com>
2024-12-30 19:51:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from dotenv import load_dotenv 
 
