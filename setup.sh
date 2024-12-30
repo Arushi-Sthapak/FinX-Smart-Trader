be4b4821bea0e372effc44406b87904320d46763
Arushi Sthapak <99334415+Arushi-Sthapak@users.noreply.github.com>
2024-12-30 20:52:32 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-# Download the latest version of chromedriver compatible with your environment
-CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
-wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-mv chromedriver /usr/local/bin/chromedriver
-chmod +x /usr/local/bin/chromedriver
-
