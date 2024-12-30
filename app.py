120e8ac32eb66d276801ef957c9c778d5a2d6791
Arushi Sthapak <99334415+Arushi-Sthapak@users.noreply.github.com>
2024-12-30 16:39:43 +0530

    Added Web scraping automation in app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 
 # Load environment variables
 load_dotenv()
